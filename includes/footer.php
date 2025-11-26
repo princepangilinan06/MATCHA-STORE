f8aa0e98735bc9721e496a8146ea57e02ac59c97
princepangilinan06 <princepangilinan06@gmail.com>
2025-11-26 14:53:33 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<?php 
+    echo '<footer>
+    <p>Created by: Prince Lenard G. Pangilinan &copy; 2025</p>
+    </footer>';
+?>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+
+    <head>
+        
+        <h1>The Matcha Store</h1>
+        <link rel='stylesheet' href='css/styles.css' />
+    </head>
+        <nav>
+            <a href="matcha.php">The Matcha Store</a>
+            <a href="if_statement.php">If Statement</a>
+            <a href="if_else.php">If-Else Statement</a>
+            <a href="if_else_if.php">If Else If Statement</a>
+            <a href="match.php">Match Expressions</a>
+            <a href="while.php">While Loop</a>
+            <a href="for_each.php">Foreach Loops</a>
+        </nav>
